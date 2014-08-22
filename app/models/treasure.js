e92d6fde6ce68e55d6e53e038f2781fae83226b9
Melanie Fryman <melanie@frymanet.com>
2014-08-21 22:07:32 -0500

    model is working!

--- app/models/treasure.js
+++ app/models/treasure.js
@@ -6,23 +6,43 @@ var Mongo = require('mongodb'),
 
 function Treasure(o){
   this.name = o.name[0];
-  this.difficulty = o.difficulty[0] * 1;
-  this.order = o.order[0] * 1;
-  // 0: name, 1: lat, 2: lng
   this.loc = {};
   this.loc.name = o.loc[0];
   this.loc.lat = parseFloat(o.loc[1]);
   this.loc.lng = parseFloat(o.loc[2]);
-  this.tags = o.tags[0].split(',').map(function(s){return s.trim();});
   this.photos = [];
+  this.difficulty = parseInt(o.difficulty[0]);
+  this.order = parseInt(o.order[0]);
+  this.tags = o.tags[0].split(',').map(function(s){return s.trim();});
   this.hints = o.hints;
   this.isFound = false;
-  this.isLinkable = this.order === 1 ? true : false;
+  this.isLinkable = this.order === 1 ? true : false; // only 1st item is active link
 }
 
 Object.defineProperty(Treasure, 'collection', {
   get: function(){return global.mongodb.collection('treasures');}
 });
+
+/*  CLASS METHODS  */
+
+Treasure.create = function(fields, files, cb){
+  var t = new Treasure(fields);
+
+  //console.log('----CREATE OBJECT START---');
+  //console.log(t);
+  //console.log('----CREATE OBJECT END---');
+
+  t.save(function(){
+    t.addPhotos(files, cb);
+  });
+};
+
+Treasure.findById = function(id, cb){
+  id = Mongo.ObjectID(id);
+  Treasure.collection.findOne({_id:id}, function(err, obj){
+    cb(obj);
+  });
+};
 
 Treasure.query = function(query, cb){
   // console.log('model.query.query', query);
@@ -35,15 +55,6 @@ Treasure.query = function(query, cb){
   Treasure.collection.find(filter).sort(sort).toArray(cb);
 };
 
-Treasure.count = function(cb){
-  Treasure.collection.count(cb);
-};
-
-Treasure.findById = function(id, cb){
-  id = Mongo.ObjectID(id);
-  Treasure.collection.findOne({_id:id}, cb);
-};
-
 Treasure.found = function(id, cb){
   id = Mongo.ObjectID(id);
   Treasure.collection.findOne({_id:id}, function(err, t){
@@ -54,12 +65,11 @@ Treasure.found = function(id, cb){
   });
 };
 
-Treasure.create = function(fields, files, cb){
-  var t = new Treasure(fields);
-  t.save(function(){
-    t.addPhotos(files, cb);
-  });
+Treasure.count = function(cb){
+  Treasure.collection.count(cb);
 };
+
+/*  INSTANCE METHODS  */
 
 Treasure.prototype.save = function(cb){
   Treasure.collection.save(this, cb);
@@ -68,18 +78,20 @@ Treasure.prototype.save = function(cb){
 Treasure.prototype.addPhotos = function(files, cb){
   var dir    = __dirname + '/../static/img/' + this._id,
       staticRoot   = '/img/' + this._id + '/',
-      exists = fs.existsSync(dir),
+      exists = fs.existsSync(dir), //true if the directory already exists
       self   = this;
 
   if(!exists){
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir); //Nodes way of making a file, uses the fs module
   }
+
   files.photos.forEach(function(photo){
     var ext = path.extname(photo.path),
         fileName = self.photos.length + ext,
         rel = staticRoot + fileName,
         abs = dir + '/' + fileName;
-    fs.renameSync(photo.path, abs);
+
+    fs.renameSync(photo.path, abs); //move and rename
     self.photos.push(rel);
   });
   self.save(cb);
--- test/unit/treasure.js
+++ test/unit/treasure.js
@@ -9,8 +9,7 @@ var expect    = require('chai').expect,
     dbConnect = require('../../app/lib/mongodb'),
     cp        = require('child_process'),
     db        = 'treasure-map-test',
-    oid       = '100000000000000000000001',
-    obj = {name: ['fancy wool'], loc:['Reykjavíc, Iceland', '64.133333', '-21.933333'], difficulty:['1'], order:['1'], hints:['hint1', 'hint2', 'hint3'], tags:['tag1, tag2, tag3']};
+    obj = {name: ['Knights'], loc:['Doune, Scotland', '56.19', '-4.053'], difficulty:['1'], order:['1'], hints:['hint1', 'hint2', 'hint3'], tags:['tag1, tag2, tag3']};
 
 describe('Treasure', function(){
   before(function(done){
@@ -21,6 +20,7 @@ describe('Treasure', function(){
 
   beforeEach(function(done){
     cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      console.log(stdout, stderr);
       done();
     });
   });
@@ -29,51 +29,44 @@ describe('Treasure', function(){
     it('should create a new Treasure object', function(){
       var t = new Treasure(obj);
       expect(t).to.be.instanceof(Treasure);
-      expect(t.name).to.equal('fancy wool');
-      expect(t.loc.name).to.equal('Reykjavíc, Iceland');
-      expect(t.loc.lat).to.be.closeTo(64.133333, 0.01);
-      expect(t.loc.lng).to.be.closeTo(-21.933333, 0.01);
+      expect(t.name).to.equal('Knights');
+      expect(t.loc.name).to.equal('Doune, Scotland');
+      expect(t.loc.lat).to.be.closeTo(56.19, 0.01);
+      expect(t.loc.lng).to.be.closeTo(-4.053, 0.01);
       expect(t.difficulty).to.equal(1);
       expect(t.order).to.equal(1);
       expect(t.tags).to.have.length(3);
       expect(t.photos).to.have.length(0);
       expect(t.hints).to.have.length(3);
       expect(t.isFound).to.equal(false);
+      expect(t.isLinkable).to.equal(true);
+      // console.log(t);
     });
   });
 
   describe('.query', function(){
     it('should get all treasures', function(done){
       Treasure.query({}, function(err, treasures){
-        expect(treasures).to.have.length(3);
-        done();
-      });
-    });
-  });
-
-  describe('.count', function(){
-    it('should return the number of treasure objects in the database', function(done){
-      Treasure.count(function(err, num){
-        expect(num).to.equal(3);
+        expect(treasures).to.have.length(6);
         done();
       });
     });
   });
 
   describe('.findById', function(){
-    it('should find a treasure object by it\'s ID', function(done){
-      Treasure.findById(oid, function(err, t){
-        expect(t._id.toString()).to.equal(oid);
+    it('should find one treasure object by it\'s ID', function(done){
+      Treasure.findById('100000000000000000000001', function(t){
+        expect(t.name).to.equal('Knights');
         done();
       });
     });
   });
 
   describe('.found', function(){
-    it('should update a treasures isFound property to true', function(done){
-      Treasure.found(oid, function(){
-        Treasure.findById(oid, function(err, t){
-          expect(t.isFound).to.equal(true);
+    it('should update a treasure\'s isFound property to true', function(done){
+      Treasure.found('100000000000000000000001', function(){
+        Treasure.findById('100000000000000000000001', function(t){ // add err if breaks
+          expect(t.isFound).to.be.true;
           done();
         });
       });
@@ -81,7 +74,7 @@ describe('Treasure', function(){
   });
 
   describe('#save', function(){
-    it('should save the object in the databse', function(done){
+    it('should save the treasure object in the databse', function(done){
       var t = new Treasure(obj);
       t.save(function(){
         expect(t._id).to.be.instanceof(Mongo.ObjectID);
